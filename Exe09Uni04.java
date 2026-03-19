3e08dd8643f2fc439de1716fe5a3152a0d41726a
Namizn <leleo2006@outlook.com>
2026-03-19 19:36:34 -0300

    Exe 10 com Switch e Case

--- Exe09Uni04.java
+++ Exe09Uni04.java
@@ -5,6 +5,7 @@ public class Exe09Uni04
     public static void main(String[] args) 
     {
         Scanner teclado = new Scanner(System.in);
+        
         System.out.println("Informe a idade do primeiro irmão:");
         int idade1 = teclado.nextInt();
         System.out.println("Informe a idade do segundo irmão:");
@@ -18,7 +19,7 @@ public class Exe09Uni04
         }
         else if (idade1 == idade2 && idade1 != idade3 || idade2 == idade3 && idade2 != idade1 || idade3 == idade1 && idade3 != idade2)
         {
-            System.out.println("São Gêmeos.");
+            System.out.println("Há dois irmãos gêmeos.");
         }
         else
         {
